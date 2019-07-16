c18f3b78bab073006cb0f8b17dcc3d1919c1b0aa
Usama Shaikh <48800317+Usamashaikh744@users.noreply.github.com>
2019-07-16 15:07:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,346 @@
+let socailNetwoerksMedia = {
+    geoNews: 
+        {
+            founder: "Muhammad Ahmed khan",
+            location: "site area",
+            description: "ARY News ARY Kahani ARY Super (Sports) and ARY Tez (Comedy)",
+            launched: "2004 december 4",
+            teamMember:
+                [
+                    "usama", "aftab", "tausif", "salman", "waqar"
+                ],
+            cameraMans:
+                [
+                    "akbar", "kabeer", "wasif"
+                ],
+            cummunicationName: [
+                "saboor", "shoukat", "shahnawaz"
+            ],
+            newspaper: "Jang Group of Newspapers",
+            timming: "morning 8'O clock",
+            salaryCameraMen:[100000, 400000, 70000],
+            carsLimit: "60000",
+            branches: [
+                "karachi",
+                "bahalwapur",
+                "nooriabad",
+                "shahpur",
+                "quetta",
+                "10+ more"
+            ],
+        },
+    
+
+    ARYNews: [
+        {
+            founder: "Abdul Razzak Yaqoob",
+            location: "korangi",
+            discription: "every moment news at a time",
+            launched: "16 july 2004",
+            teamMember:
+                [
+                    "ali", "daniyal", "ahmeed", "ashraf", "waqar"
+                ],
+            cameraMans:
+                [
+                    "wasiq", "saleem", "umer"
+                ],
+            cummunicationName:
+                [
+                    "sarim", "fawad", "shahzaib"
+                ],
+            newspaper: "ary Group of Newspapers",
+            timming: "morning 7'O clock",
+            salaryCameraMen:
+                [
+                    50000, 30000, 60000
+                ],
+            carsLimit: "thousands",
+            branches: [
+                "karachi",
+                "lahore",
+                "shahdadpor",
+                "queeta",
+                "islamabad",
+                "10+ more"
+            ],
+        },
+    ],
+
+    samaaNews: [
+        {
+            founder: "sheheroz khan",
+            location: "nazmabad",
+            discription: "hr khabar pr nazar",
+            launched: "20 august 2008",
+            teamMember:
+                [
+                    "ali", "daniyal", "ahmeed", "ashraf", "waqar"
+                ],
+            cameraMans:
+                [
+                    "akbar", "kabeer", "wasif"
+                ],
+            cummunicationName:
+                [
+                    "sarim", "fawad", "shahzaib"
+                ],
+            newspaper: "samaa Group of Newspapers",
+            timming: "morning 7'O clock",
+            salaryCameraMen: [50000, 30000, 60000],
+            carsLimit: 1000,
+            branches: [
+                "karachi",
+                "lahore",
+                "shahdadpor",
+                "queeta",
+                "islamabad",
+                "10+ more"
+            ],
+        },
+
+    ],
+
+    duniaNews: [
+        {
+            founder: "tahir baloch",
+            location: "XYZ",
+            discription: "dunia News dunia Kahani dunia Super (Sports)",
+            launched: "16 july 2004",
+            teamMember:
+                [
+                    "ali", "daniyal", "ahmeed", "ashraf", "waqar"
+                ],
+            cameraMans:
+                [
+                    "akbar", "kabeer", "wasif"
+                ],
+            cummunicationName:
+                [
+                    "sarim", "fawad", "shahzaib"
+                ],
+            newspaper: "dunia Group of Newspapers",
+            timming: "morning 7'O clock",
+            salaryCameraMen: [50000, 30000, 60000],
+            carsLimit: "thousands",
+            branches: [
+                "karachi",
+                "lahore",
+                "shahdadpor",
+                "queeta",
+                "islamabad",
+                "10+ more"
+            ],
+        },
+    ],
+
+    expressNews: [
+        {
+            founder: "maqsood shah",
+            location: "XYZ",
+            discription: " express News express Kahani",
+            launched: "16 july 2004",
+            teamMember: [
+                "ali", "daniyal", "ahmeed", "ashraf", "waqar"
+            ],
+            cameraMans:
+                [
+                    "akbar", "kabeer", "wasif"
+                ],
+            cummunicationName:
+                [
+                    "sarim", "fawad", "shahzaib"
+                ],
+            newspaper: "express Group of Newspapers",
+            timming: "morning 7'O clock",
+            salaryCameraMen: [50000, 30000, 60000],
+            carsLimit: "thousands",
+            branches: [
+                "karachi",
+                "lahore",
+                "shahdadpor",
+                "queeta",
+                "islamabad",
+                "10+ more"
+            ],
+        },
+    ],
+}
+
+let socialNetworkApp = {
+
+    facebook: [
+        {
+            socailApp: "facebook",
+            owner: "marke zukerberg"
+        },
+        {
+            usedTo: "about sharing idea",
+            launch: "February 2004, Cambridge, Massachusetts, United States",
+        },
+        facebookData = [
+            {
+                firstName: "usaid",
+                lastName: "shaikh",
+                dateOfBirth: "20 july 2000",
+                email: "sfaagg@gamil.com",
+                phoneNumber: 07343703737,
+                profilePhoto: " 236236 39gfhd",
+                password: "**********",
+                retypePassword: "************",
+            },
+        ],
+
+    ],
+
+    whatsApp: [
+        {
+            socailApp: "whatsApp",
+            owner: "Jan Koum",
+            founder: " Ukrainian American ",
+        },
+
+        {
+            usedTo: "about others countries to communicate easily",
+            launch: " 2012",
+        },
+
+        whatsAppData = [
+            {
+                sender: "usaid",
+                time: new Date(),
+                message: "hello world",
+
+            },
+
+            {
+                sender: "usama",
+                time: new Date(),
+                message: "hello world",
+            }
+
+        ],
+        contacts = [
+            { 
+                number: 0332-2650769 ,
+                number: 0332-2650769 ,
+                number: 0332-2650769 
+            },
+
+            { 
+                number: 0333-5472879 ,
+                number: 0333-5472879 ,
+                number: 0333-5472879 
+            },
+        ]
+    ],
+
+    twitter: [
+        {
+            socailApp: "twitter",
+            owner: "Jack dorsey",
+        },
+
+        {
+            usedTo: "about sharing idea",
+            launch: " March 21, 2006, San Francisco, California, United States",
+        },
+
+        {
+            bornOn: "February 24, 1976",
+            dateOfDeath: "XYZ",
+        },
+
+        twitterData = [
+            {
+                firstName: "usaid",
+                lastName: "shaikh",
+                dateOfBirth: "20 july 2000",
+                email: "sfaagg@gamil.com",
+                phoneNumber: 07343703737,
+                profilePhoto: " 236236 39gfhd",
+                password: "**********",
+                retypePassword: "************",
+            },
+        ],
+    ],
+
+    instagram: [
+        {
+            socailApp: "Instagram",
+            owner: " Kevin Systrom",
+        },
+
+        {
+            usedTo: "video-sharing social networking service",
+            launch: " October 6, 2010",
+        },
+
+        {
+            bornOn: "november 21, 1976",
+            dateOfDeath: "XYZ",
+        },
+
+        instagramData = [
+            {
+                firstName: "usaid",
+                lastName: "shaikh",
+                dateOfBirth: "20 july 2000",
+                email: "sfaagg@gamil.com",
+                phoneNumber: 07343703737,
+                profilePhoto: " 236236 39gfhd",
+                password: "**********",
+                retypePassword: "************",
+            },
+        ],
+    ],
+
+    linkedin: [
+        {
+            socailApp: "linkedin",
+            owner: "Reid Hoffman",
+        },
+
+        {
+            usedTo: "its used to developers",
+            launch: "2002, Mountain View, California, United States",
+        },
+        linkedkData = [
+            {
+                firstName: "usaid",
+                lastName: "shaikh",
+                dateOfBirth: "20 july 2000",
+                email: "sfaagg@gamil.com",
+                phoneNumber: 07343703737,
+                profilePhoto: " 236236 39gfhd",
+                password: "**********",
+                retypePassword: "************",
+            },
+        ],
+    ],
+
+    skype: [
+        {
+            socailApp: "skype",
+            owner: " Niklas Zennström",
+        },
+
+        {
+            usedTo: "easily vide call to cummunication",
+            launch: "2006 California, USA 94301",
+        },
+
+        skypeData = [
+            {
+                firstName: "usaid",
+                lastName: "shaikh",
+                dateOfBirth: "20 july 2000",
+                email: "sfaagg@gamil.com",
+                phoneNumber: 07343703737,
+                profilePhoto: " 236236 39gfhd",
+                password: "**********",
+                retypePassword: "************",
+            },
+        ],
+    ],
+}
